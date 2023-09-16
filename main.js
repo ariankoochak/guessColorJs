c6958d9d251f892a4d047c6c6689d853262563f4
Arian <45326611+ariankoochak@users.noreply.github.com>
2023-09-16 18:42:05 +0330

    add endGameFlag

--- main.js
+++ main.js
@@ -1,41 +1,43 @@
-const mainColor = document.querySelector('#main-color')
-const colors = document.querySelectorAll('.color')
-const container = document.querySelector('.container')
-const newGameBtn = document.querySelector('button')
-let colorCodeArr = []
+const mainColor = document.querySelector("#main-color");
+const colors = document.querySelectorAll(".color");
+const container = document.querySelector(".container");
+const newGameBtn = document.querySelector("button");
+let colorCodeArr = [];
+let endGameFlag = false;
 
 function getRandomNumber() {
     return Math.floor(Math.random() * 255);
 }
 
-newGameBtn.addEventListener('click',function(){
-    colorCodeArr = []
-    newGameBtn.style.display = 'none';
-    container.style.backgroundColor = 'black';
+newGameBtn.addEventListener("click", function () {
+    colorCodeArr = [];
+    newGameBtn.style.display = "none";
+    container.style.backgroundColor = "black";
+    endGameFlag = false;
     prepareGame();
-})
+});
 
-function prepareGame(){
+function prepareGame() {
     for (const color of colors) {
         color.style.visibility = "visible";
         const template = `rgb(${getRandomNumber()}, ${getRandomNumber()}, ${getRandomNumber()})`;
         colorCodeArr.push(template);
         color.style.backgroundColor = template;
         color.addEventListener("click", function () {
-            if (color.style.backgroundColor == mainColor.textContent) {
-                container.style.backgroundColor = color.style.backgroundColor;
-                newGameBtn.style.display = 'inline-block';
-            } else {
-                color.style.visibility = "hidden";
+            if (endGameFlag == false) {
+                if (color.style.backgroundColor == mainColor.textContent) {
+                    container.style.backgroundColor = color.style.backgroundColor;
+                    endGameFlag = true
+                    newGameBtn.style.display = "inline-block";
+                } else {
+                    color.style.visibility = "hidden";
+                }
             }
         });
     }
-    mainColor.textContent = colorCodeArr[Math.floor(Math.random() * colorCodeArr.length)];
+    mainColor.textContent =
+        colorCodeArr[Math.floor(Math.random() * colorCodeArr.length)];
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 prepareGame();
-
-
-
-
